a0ed6b9db245150cd2c483797554eeab7cb82b6b
Christopher Møller <cmo@frederikshavnfriskole.dk>
2026-03-04 08:57:32 +0100

    test(production): snapshot progression policy v1.0.0

---
+++ production/__tests__/progressionSnapshot.test.ts
@@ -0,0 +1,19 @@
+﻿import { generateProductionPlan } from "../progressionPolicy";
+
+describe("progression snapshot", () => {
+  it("should generate stable plans for known objectives", () => {
+    const objectives = [
+      "ww2_poland_invasion_start_year",
+      "ww2_poland_invasion_impact",
+      "ww2_france_fall_role",
+      "ww2_france_fall_turning_point",
+    ];
+
+    const plans = objectives.map((o) => ({
+      objective: o,
+      plan: generateProductionPlan(o),
+    }));
+
+    expect(plans).toMatchSnapshot();
+  });
+});
