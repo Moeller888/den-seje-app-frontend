54595aa40fc8ca25626a17fe9231f9d637e22ec1
Christopher Møller <cmo@frederikshavnfriskole.dk>
2026-03-24 09:12:08 +0100

    fix: single supabase client (remove duplicate auth)

--- js/app.js
+++ js/app.js
@@ -1,11 +1,4 @@
-﻿import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
-
-const supabase = createClient(
-  "https://tjzbehwfagiwpwodsgwg.supabase.co",
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRqemJlaHdmYWdpd3B3b2RzZ3dnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzE2ODc5OTQsImV4cCI6MjA4NzI2Mzk5NH0.BzepnYLe6Khzqx9vTL3Ifa_zMRgjoGQ9Lw5seaoKMMc"
-);
-
-const UI_STATES = {
+﻿const UI_STATES = {
   LOADING_QUESTION: "LOADING_QUESTION",
   AWAITING_ANSWER: "AWAITING_ANSWER",
 };
